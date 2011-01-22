14ab59a81b39083c3bbd205d0dce5b7cbde71eeb
Kirby Turner <kirby@thecave.com>
2011-01-22 15:41:57 -0500

    Added very base unit tests.

---
+++ MacTests/Tests/KTCsvParserTestCases.h
@@ -0,0 +1,20 @@
+//
+//  KTCsvParserTestCases.h
+//  MacTests
+//
+//  Created by Kirby Turner on 1/22/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface KTCsvParserTestCases : SenTestCase 
+{
+
+}
+
+- (void)testSimpleLineParse;
+- (void)testEmbeddedQuotes;
+
+@end
---
+++ MacTests/Tests/KTCsvParserTestCases.m
@@ -0,0 +1,40 @@
+//
+//  KTCsvParserTestCases.m
+//  MacTests
+//
+//  Created by Kirby Turner on 1/22/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import "KTCsvParserTestCases.h"
+#import "KTCsvParser.h"
+
+
+@implementation KTCsvParserTestCases
+
+- (void)testSimpleLineParse
+{
+   NSString *csvString = @"a, b, c";
+   NSArray *values = [KTCsvParser valuesFromCsvLine:csvString withValueSeparator:@","];
+   STAssertTrue([values count] == 3, @"Unexpected count.");
+   STAssertTrue([[values objectAtIndex:0] isEqualToString:@"a"], @"Unexpected value.");
+   STAssertTrue([[values objectAtIndex:1] isEqualToString:@" b"], @"Unexpected value.");
+   STAssertTrue([[values objectAtIndex:2] isEqualToString:@" c"], @"Unexpected value.");
+}
+
+- (void)testEmbeddedQuotes
+{
+   // Test reading a single line.
+   NSString *csvString;
+   csvString = @"a,\"\"\"b\"\"\",c\r1,2,3";
+   NSData *data = [csvString dataUsingEncoding:NSUTF8StringEncoding];
+   
+   // Simulate reading a file.
+   NSInputStream *inputStream = [NSInputStream inputStreamWithData:data];
+   KTCsvParser *parser = [[KTCsvParser alloc] initWithInputStream:inputStream];
+   while ([parser readLine]) {
+      NSLog (@"values: %@", [parser values]);
+   }
+   [parser release];
+}
+@end
